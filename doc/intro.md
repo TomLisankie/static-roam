b007899abd36152a28db4ae8e6b166dbfe1780bc
Thomas Lisankie <id@tomlisankie.com>
2020-03-19 18:38:50 -0400

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to roaman-life-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/roaman_life_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns roaman-life-clj.core-test
+  (:require [clojure.test :refer :all]
+            [roaman-life-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
