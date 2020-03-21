5f79904d283b5e7fc2b9bd1edf18df795abcaec8
Thomas <thomas.lisankie@gmail.com>
2020-03-21 16:09:49 -0400

    Now properly unzips a Roam JSON archive

--- src/roaman_life_clj/core.clj
+++ src/roaman_life_clj/core.clj
@@ -1,5 +1,26 @@
-(ns roaman-life-clj.core)
+(ns roaman-life-clj.core
+  (:require [me.raynes.fs :as fs]
+        [clojure.java.io :as io])
+  (:import (java.util.zip ZipFile)))
 
 (def ZIP-DIR "/home/thomas/Dropbox/Roam Exports/")
 (def ZIP-NAME "roam-test-export.zip")
 
+(defn unzip-roam-json-archive
+  "Takes the path to a zipfile `source` and unzips it to target-dir, returning the path of the target file"
+  [source target-dir]
+  (str target-dir (with-open [zip (ZipFile. (fs/file source))]
+                    (let [entries (enumeration-seq (.entries zip))
+                          target-file #(fs/file target-dir (str %))
+                          database-file-name (.getName (first entries))]
+                      (doseq [entry entries :when (not (.isDirectory ^java.util.zip.ZipEntry entry))
+                              :let [f (target-file entry)]]
+                        (fs/mkdirs (fs/parent f))
+                        (io/copy (.getInputStream zip entry) f))
+                      database-file-name))))
+
+(defn main
+  []
+  (unzip-roam-json-archive (str ZIP-DIR ZIP-NAME) ZIP-DIR))
+
+(main)
