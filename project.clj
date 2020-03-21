bfbc6842028ee0e204125c3309152a038c9c045e
Thomas Lisankie <id@tomlisankie.com>
2020-03-21 15:21:44 -0400

    Updated dependenices and wiped core.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [me.raynes/fs "1.4.6"]])
--- src/roaman_life_clj/core.clj
+++ src/roaman_life_clj/core.clj
@@ -3,23 +3,3 @@
 (def ZIP-DIR "/home/thomas/Dropbox/Roam Exports/")
 (def ZIP-NAME "roam-test-export.zip")
 
-(defn ^File file
-  "If `path` is a period, replaces it with cwd and creates a new File object
-   out of it and `paths`. Or, if the resulting File object does not constitute
-   an absolute path, makes it absolutely by creating a new File object out of
-   the `paths` and cwd."
-  [path & paths]
-  (when-let [path (apply
-                   io/file (if (= path ".")
-                             *cwd*
-                             path)
-                   paths)]
-    (if (.isAbsolute ^File path)
-      path
-      (io/file *cwd* path))))
-
-(defn unzip
-  "Takes the path to a ZIP file `source` and unzips it to `target-dir`"
-  [source target-dir]
-  (with-open [])
-  )
