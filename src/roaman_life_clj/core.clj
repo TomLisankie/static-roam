07a237fc48d09e232f9783d2ba82d4ecfb19355e
Thomas <thomas.lisankie@gmail.com>
2020-03-21 15:10:22 -0400

    Some progress

--- src/roaman_life_clj/core.clj
+++ src/roaman_life_clj/core.clj
@@ -1,6 +1,8 @@
 (ns roaman-life-clj.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ZIP-DIR "/home/thomas/Dropbox/Roam Exports/")
+(def ZIP-NAME "roam-test-export.zip")
+
+(defn unzip
+  "Takes the path to a ZIP file `source` and unzips it to `target-dir`"
+  )
