d0d257787e468eff38f82bdc2dc4f10800698909
Thomas <thomas.lisankie@gmail.com>
2020-03-31 14:07:41 -0400

    Added Hiccup and Stasis as dependencies

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [me.raynes/fs "1.4.6"]
-                 [org.clojure/data.json "1.0.0"]])
+                 [org.clojure/data.json "1.0.0"]
+                 [hiccup "1.0.5"]
+                 [stasis "2.5.0"]])
