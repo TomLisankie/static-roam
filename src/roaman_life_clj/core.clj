374ab13744ce312789bd7dcf31b9635f899e9bf4
Thomas <thomas.lisankie@gmail.com>
2020-04-04 17:33:05 -0400

    Some changes to child bullet template

--- src/roaman_life_clj/core.clj
+++ src/roaman_life_clj/core.clj
@@ -80,17 +80,17 @@
   [blockish]
   (loop [children (when (:children blockish)
                     (:children blockish))
-         html [""]]
+         html [:ul]]
     (if (= 0 (count children))
       html
-      (recur (rest children) (conj html
-                                   [:ul [:li (:string (first children))]
-                                    (if (:children (first children))
-                                      (children-list-template (first children))
-                                      nil)])))))
+      (recur (rest children) (conj html (if (:children (first children))
+                                          (concat [:ul [:li (:string (first children))]] (children-list-template (first children)))
+                                          [:li (:string (first children))]))))))
 
 (defn page-template
   [page] ;; each indent level is a new ul. Each element in an indent level is a new li
+  (when (= (:title page) "RL Blog Post")
+    (json/pprint (:children (last page))))
   (into [] (concat [:div [:h1 (:title page)]] (children-list-template page))))
 
 (defn -main
@@ -103,5 +103,7 @@
         included-pages-to-mentioned-pages-map (zipmap (map #(:title %) posts) (map #(pages-mentioned-by-children % title-to-content-map) (map #(:title %) posts)))
         titles-of-included-pages (find-all-included-pages (map #(:title %) posts) 5 title-to-content-map)
         included-title-to-content-map (zipmap titles-of-included-pages (map #(get title-to-content-map %) titles-of-included-pages))]
-    (stasis/export-pages {"/test.html" (hiccup/html (map page-template (filter #(not= nil (:title %)) (vals included-title-to-content-map))))} ".")
-    (json/pprint (vals included-title-to-content-map))))
+    ;; (stasis/export-pages {"/test.html" (hiccup/html (map page-template (filter #(not= nil (:title %)) (vals included-title-to-content-map))))} ".")
+    ;; (json/pprint (vals included-title-to-content-map))
+    (get included-title-to-content-map "Fitness")
+    ))
