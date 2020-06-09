ce2d4c1918e500a6dd9b321dd5380453040601a1
Thomas Lisankie <id@tomlisankie.com>
2020-06-08 22:25:57 -0400

    Now loads grammar from the EBNF file

--- src/static_roam/parser.clj
+++ src/static_roam/parser.clj
@@ -9,53 +9,10 @@
 ;; Instaparse docs: https://github.com/Engelberg/instaparse#readme
 
 (defparser block-parser
-  "(* This first rule is the top-level one. *)
-   block = ( syntax-in-block / any-char )*
-   (* `/` ordered alternation is used to, for example, try to interpret a string beginning with '[[' as a page-link before interpreting it as raw characters. *)
-
-   <syntax-in-block> = (special-action | code-line | metadata-tag | page-link | block-ref | hashtag | url-link | bold | italic | highlight | strikethrough)
-
-   page-link = <'[['> any-chars <']]'>
-
-   block-ref = <'(('> #'[a-zA-Z0-9_\\-]+' <'))'>
-
-   metadata-tag = any-chars <'::'>
-
-   code-line = <'`'> any-chars <'`'>
-
-   <special-action> = (query | youtube-embed | word-count) (* <'{{'> any-chars <'}}'> *)
-
-   query = <'{{query: '> any-chars <'}}'>
-   youtube-embed = <'{{youtube: '> any-chars <'}}'>
-   word-count = <'{{word-count}}'>
-
-   hashtag = hashtag-bare | hashtag-delimited
-   <hashtag-bare> = <'#'> #'[\\p{L}\\p{M}\\p{N}_]+'  (* Unicode: L = letters, M = combining marks, N = numbers *)
-   <hashtag-delimited> = <'#'> <'[['> #'[^\\]]+' <']]'>
-
-   url-link = url-link-text url-link-url
-   <url-link-text> = <'['> url-link-text-contents <']'>
-   url-link-text-contents = ( (bold | backslash-escaped-right-bracket) / any-char )*
-   <backslash-escaped-right-bracket> = <'\\\\'> ']'
-   <url-link-url> = <'('> url-link-url-parts <')'>
-   url-link-url-parts = url-link-url-part+
-   <url-link-url-part> = (backslash-escaped-paren | '(' url-link-url-part* ')') / any-char
-   <backslash-escaped-paren> = <'\\\\'> ('(' | ')')
-
-   bold = <'**'> any-chars <'**'>
-   italic = <'__'> any-chars <'__'>
-   highlight = <'^^'> any-chars <'^^'>
-   strikethrough = <'~~'> any-chars <'~~'>
-
-   (* It’s useful to extract this rule because its transform joins the individual characters everywhere it’s used. *)
-   (* However, I think in many cases a more specific rule can be used. So we will migrate away from uses of this rule. *)
-   any-chars = any-char+
-
-   <any-char> = #'\\w|\\W'
-   ")
+  (slurp "src/static_roam/parser.ebnf"))
 
 
-(defn combine-adjacent-strings
+(defn- combine-adjacent-strings
   "In a sequence of strings mixed with other values, returns the same sequence with adjacent strings concatenated.
    (If the sequence contains only strings, use clojure.string/join instead.)"
   [coll]
@@ -70,8 +27,8 @@
     coll))
 
 
-(defn transform-to-ast
-  "Transforms the Instaparse output tree to an abstract syntax tree for Athens markup."
+(defn- transform-to-ast
+  "Transforms the Instaparse output tree to an abstract syntax tree for SR markup."
   [tree]
   (insta/transform
     {:block                  (fn [& raw-contents]
@@ -90,7 +47,7 @@
 
 (defn parse-to-ast
   "Converts a string of block syntax to an abstract syntax tree for Athens markup."
-  [string]
-  (transform-to-ast (block-parser string)))
+  [block-content]
+  (transform-to-ast (block-parser block-content)))
 
-(parse-to-ast "{{youtube: https://youtu.be/5iI_0wnwIpU}}")
+(parse-to-ast "[[asdfasdf]] [[assd]]")
