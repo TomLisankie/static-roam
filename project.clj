e630bd210e6b666dc6f5303dedb3208d0cbcb44a
Thomas Lisankie <id@tomlisankie.com>
2020-08-13 21:11:10 -0800

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject static-roam "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject static-roam "0.0.1-alpha"
+  :description "A static-site generator for Roam Research"
+  :url "https://github.com/TomLisankie/static-roam"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
