87c70df2ac26cad0b4f783c0542e815570cd5ca1
Thomas Lisankie <id@tomlisankie.com>
2021-01-26 19:55:45 -0500

    Further modelling of config

--- src/static_roam/template_config.clj
+++ src/static_roam/template_config.clj
@@ -1,51 +1,54 @@
 (ns static-roam.template-config)
 
+;; Keep in mind, all of these are only operating on blocks that we've already marked as included
 {
- "about"
- {
-  :folder "/about"
-  :index true
-  :query "PageTitle: About"
-  :query '(tagged-as "About")
-  }
- "contact"
- {
-  :folder "/contact"
-  :index true
-  :query "PageTitle: Contact"
-  }
- "graph-page-example"
- {
-  :folder "/nodes"
-  :query "Not:Post,About,Homepage,Contact,Now"
-  }
- "index"
- {
-  :folder "/"
-  :index true
-  :query "PageTitle: Homepage"
-  }
- "mind"
- {
-  :folder "/mind"
-  :index true
-  :query "TaggedAs: EntryPoint"
-  }
- "now"
- {
-  :folder "/now"
-  :index true
-  :query "PageTitle: Now"
-  }
- "post-example"
- {
-  :folder "/nodes"
-  :query "TaggedAs: Post"
-  }
- "posts"
- {
-  :folder "/posts"
-  :index true
-  :query "TaggedAs: Post"
-  }
+ :exclude ["Exclude", "high"]
+ :include ["Include"]
+ :templates {"about"
+             {
+              :folder "/about"
+              :index true
+              :query "TaggedAs: About"
+              }
+             "contact"
+             {
+              :folder "/contact"
+              :index true
+              :query "TaggedAs: Contact"
+              }
+             "graph-page-example"
+             {
+              :folder "/nodes"
+              :query "Exclude:Post,About,Homepage,Contact,Now"
+              }
+             "index"
+             {
+              :folder "/"
+              :index true
+              :query "TaggedAs: Homepage"
+              }
+             "mind"
+             {
+              :folder "/mind"
+              :index true
+              :query "TaggedAs: EntryPoint"
+              }
+             "now"
+             {
+              :folder "/now"
+              :index true
+              :query "TaggedAs: Now"
+              }
+             "post-example"
+             {
+              :folder "/nodes"
+              :query "TaggedAs: Post"
+              }
+             "posts"
+             {
+              :folder "/posts"
+              :index true
+              :query "TaggedAs: Post"
+              }
+             }
  }
