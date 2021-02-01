9227cd596db4bfce5c035fc104f2889d92e1a66f
Thomas Lisankie <id@tomlisankie.com>
2021-02-01 12:28:09 -0500

    Function heads for templates

--- src/static_roam/templates.clj
+++ src/static_roam/templates.clj
@@ -1,20 +1,44 @@
 (ns static-roam.templates)
 
+(defn about-template
+  [roam-db])
+
+(defn contact-template
+  [roam-db])
+
+(defn graph-page-template
+  [roam-db])
+
+(defn homepage-template
+  [roam-db])
+
+(defn mind-template
+  [roam-db])
+
+(defn now-template
+  [roam-db])
+
+(defn post-template
+  [roam-db])
+
+(defn posts-index-template
+  [roam-db])
+
 (def template-fns
   {"about"
-   (fn [roam-db page-eid])
+   about-template
    "contact"
-   (fn [roam-db page-eid])
+   contact-template
    "graph-page-example"
-   (fn [roam-db page-eid])
+   graph-page-template
    "index"
-   (fn [roam-db page-eid])
+   homepage-template
    "mind"
-   (fn [roam-db page-eid])
+   mind-template
    "now"
-   (fn [roam-db page-eid])
+   now-template
    "post-example"
-   (fn [roam-db page-eid])
+   post-template
    "posts"
-   (fn [roam-db page-eid])
+   posts-index-template
    })
