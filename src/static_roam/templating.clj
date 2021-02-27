2f707e5e9c1996e8d2f3e55fbcaaf23b5e1cb029
Mike Travers <mtravers@parkerici.org>
2021-02-26 22:24:33 -0800

    switch to column template from boostrap samples

--- src/static_roam/templating.clj
+++ src/static_roam/templating.clj
@@ -35,6 +35,8 @@
         nav-bar-page-dict (zipmap nav-bar-hrefs nav-bar-pages-r)]
     nav-bar-page-dict))
 
+;;; Old version
+#_
 (defn page-hiccup
   [body-hiccup page-title block-map]
   [:html
@@ -68,27 +70,22 @@
   [block-id]
   (str config/roam-base-url block-id))
 
-
 (defn block-template
-  [block-id block-map]
-  (let [properties (get block-map block-id)]
-    [:ul {:id block-id}
+  [block-id block-map & [depth]]
+  (let [depth (or depth 0)
+        properties (get block-map block-id)]
+    [:ul {:id block-id :class (if (< depth 2) "nondent" "")} ;don't indent the first 2 levels
      (if (or (nil? (:hiccup properties))
              (= (:content properties) block-id))
        ""
        [:li.block {:onclick (when config/dev-mode
                               #_ (str "location.href='" (roam-url block-id) "'")
-                              (format "window.open('%s', '_blank', 'location=yes,scrollbars=yes,status=yes');"
-                                      (roam-url block-id) ;TODO prob want page url
-                                      )
+                              (format "window.open('%s', '_blank', 'location=yes,scrollbars=yes,status=yes');" (roam-url block-id)) ;TODO prob want page url
                               )}
         (:hiccup properties)])
-     (let [children (:children properties)]
-       (if (not= 0 (count children))
-         ;; recurse on each of the children
-         (map #(block-template % block-map) children)
-         ;; otherwise, evaluate to empty div
-         [:div]))]))
+     (map #(block-template % block-map (inc depth))
+          (:children properties))
+     ]))
 
 (defn- get-parent
   [block-id block-map]
@@ -119,6 +116,8 @@
   (concat []
           (map (partial linked-reference-template block-map) references)))
 
+;;; Old version 
+#_
 (defn block-page-template
   [block-id block-map]
   (let [block (get block-map block-id)
@@ -135,6 +134,99 @@
          [:div.incoming
           [:h3 "Incoming links"]
           (linked-references-template linked-refs block-map)]))]))
+
+;;; Boostrap template version
+(defn page-hiccup
+  [body-hiccup page-title block-map]
+  `[:html
+    [:head
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+     [:title ~page-title]               ;TODO might want to prepend a site title
+     [:link {:rel "stylesheet"
+             :href "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+             :integrity "sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+             :crossorigin "anonymous"}]
+     ~@(for [css config/site-css]
+         `[:link {:rel "stylesheet" :href ~css}])
+     [:link {:rel "preconnect" :href "https://fonts.gstatic.com"}]
+     [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"}]]
+  [:body
+   [:nav.navbar.navbar-expand-lg.navbar-dark.bg-dork.fixed-top
+    [:div.container
+     [:a.navbar-brand {:href "#"} "Agency Made Me Do it"]
+     [:button.navbar-toggler
+      {:type "button",
+       :data-toggle "collapse",
+       :data-target "#navbarResponsive",
+       :aria-controls "navbarResponsive",
+       :aria-expanded "false",
+       :aria-label "Toggle navigation"}
+      [:span.navbar-toggler-icon]]
+     [:div.collapse.navbar-collapse
+      {:id "navbarResponsive"}
+      [:ul.navbar-nav.ml-auto
+       [:li.nav-item.active
+        [:a.nav-link {:href "#"} "Home\n              " [:span.sr-only "(current)"]]]
+       [:li.nav-item [:a.nav-link {:href "#"} "About"]]
+       [:li.nav-item [:a.nav-link {:href "#"} "Services"]]
+       [:li.nav-item [:a.nav-link {:href "#"} "Contact"]]]]]]
+   [:div.container
+    ~body-hiccup]
+   "<!-- Footer -->"
+   [:footer.py-5.bg-dork
+    [:div.container [:p.m-0.text-center.text-white "Copyright © Hyperphor 2020"]]
+    ]]])
+
+
+;;; Boostrap version
+(defn block-page-template
+  [block-id block-map]
+  (let [block (get block-map block-id)
+        title (parser/block-content->hiccup (get block :content) block-map) ;no this makes no sense
+        contents (block-template block-id block-map)]
+    [:div.main
+     [:div.row
+      "<!-- Post Content Column -->"
+      [:div.col-lg-8
+       "<!-- Title -->"
+       [:h1.mt-4.ptitle title]
+       [:hr {}]
+       contents
+       [:hr {}]
+       ]
+      "<!-- Sidebar Widgets Column -->"
+      [:div.col-md-4
+       "<!-- Search Widget -->"
+       [:div.card.my-4
+        [:h5.card-header "Search"]
+        [:div.card-body
+         [:div.input-group
+          [:input.form-control {:type "text", :placeholder "Search for..."}]
+          [:span.input-group-append [:button.btn.btn-secondary {:type "button"} "Go!"]]]]]
+       "<!-- Categories Widget -->"
+       [:div.card.my-4
+        [:h5.card-header "Categories"]
+        [:div.card-body
+         [:div.row
+          [:div.col-lg-6
+           [:ul.list-unstyled.mb-0
+            [:li {} [:a {:href "#"} "Web Design"]]
+            [:li {} [:a {:href "#"} "HTML"]]
+            [:li {} [:a {:href "#"} "Freebies"]]]]
+          [:div.col-lg-6
+           [:ul.list-unstyled.mb-0
+            [:li {} [:a {:href "#"} "JavaScript"]]
+            [:li {} [:a {:href "#"} "CSS"]]
+            [:li {} [:a {:href "#"} "Tutorials"]]]]]]]
+       "<!-- Side Widget -->"
+       [:div.card.my-4
+        [:h5.card-header "Side Widget"]
+        [:div.card-body
+         "You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!        "]]]]
+     "<!-- /.row -->"]
+    ))
+
 
 (defn home-page-hiccup
   [entry-points block-map]
