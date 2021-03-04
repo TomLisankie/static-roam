9de1efd7486874ab9bdbeec4d774e354e8e09fde
Mike Travers <mtravers@parkerici.org>
2021-03-04 15:56:17 -0800

    prettify recent changes page

--- src/static_roam/recent.clj
+++ src/static_roam/recent.clj
@@ -21,17 +21,22 @@
 
 (defn recent-page-content
   [block-map]
-  `[:div.main
-    ;; TODO prob needs row/col stuff
-    [:h1.ptitle "Recent changes"]
-    ~@(for [group (recents block-map)
-            :let [page-id (:page (first group))
-                  edit-time (reduce max 0 (map :edit-time group))]]
-        `[:div
-          "From " ~(parser/page-link (get block-map page-id))
-          " "
-          [:span ~(utils/render-time edit-time)]
-          ~@(for [block (take 3 group)] ;limit to 3 chunks
-              [:div (templating/block-template (:id block) block-map)])])])
+  [:div.main
+   ;; TODO prob needs row/col stuff
+   [:div.row
+    [:div.col-lg-8                     ;TODO no side col I guess
+     [:div.ptitle
+      [:h1 "Recent changes"]
+      [:hr]
+      (for [group (recents block-map)
+             :let [page-id (:page (first group))
+                   edit-time (reduce max 0 (map :edit-time group))]]
+         [:div
+          [:div.pheader
+           "from " (parser/page-link (get block-map page-id))
+           " "
+           [:span (utils/render-time edit-time)]]
+          (for [block (take 3 group)] ;limit to 3 chunks
+            [:dAiv (templating/block-template (:id block) block-map)])])]]]])
 
 
