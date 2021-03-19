2502ae3434971e59c26da00f5401cc4e68c1e2dc
Mike Travers <mtravers@parkerici.org>
2021-03-18 21:03:58 -0700

    graph displaying node titles, badly

--- src/static_roam/graph.clj
+++ src/static_roam/graph.clj
@@ -18,7 +18,7 @@
                    (filter :page?)
                    (map (fn [index block] (assoc block
                                                  :index index
-                                                 :page-refs (db/page-refs block-map block)))
+                                                 :page-refs (db/page-refs block)))
                         (range)))
         indexed (u/index-by :id pages)] ;make a new block map...
     indexed))
@@ -29,15 +29,20 @@
   (let [pages (->> block-map
                    vals
                    (filter :page?)
+                   (filter :include?)
                    (map (fn [index block] (assoc block
                                                  :index index
-                                                 :page-refs (db/page-refs block-map block)))
+                                                 :page-refs (db/page-refs block)))
                         (range)))
         indexed (u/index-by :id pages)] ;make a new block map...
     [{:name "node-data"
       :values (map (fn [b]
                      ;;  :size (- 20 (or (:depth b) 0)) (not working)
-                     {:name (:id b) :group (if (:include? b) 1 8) :index (:index b)})
+                     {:name (:id b)
+                      :index (:index b)
+;                      :group (if (:include? b) 1 8)
+                      :size (- 300 (* 30 (or (:depth b) 0)))
+                      })
                    pages)}
      {:name "link-data"
       :values (remove #(nil? (:target %))
@@ -52,83 +57,107 @@
   [block-map]
   `{:description
     "A node-link diagram with force-directed layout, depicting character co-occurrence in the novel Les Misérables.",
-    :data
-    ~(graph-data block-map)
-    :autosize "none",
-    :width 700,
+    :$schema "https://vega.github.io/schema/vega/v5.json"
+    :data ~(graph-data block-map)
+    :autosize "none"
+    :width 700
     :scales
-    [{:name "color",
-      :type "ordinal",
-      :domain {:data "node-data", :field "group"},
+    [{:name "color"
+      :type "ordinal"
+      :domain {:data "node-data" :field "group"}
       :range {:scheme "set1"}
-      }],
-    :padding 0,
+      }]
+    :padding 0
     :marks
-    [{:name "nodes",
-      :type "symbol",
-      :zindex 1,
-      :from {:data "node-data"},
+    [{:name "nodes"
+      :type "symbol"
+      :zindex 1
+      :from {:data "node-data"}
       :on
-      [{:trigger "fix",
-        :modify "node",
-        :values "fix === true ? {fx: node.x, fy: node.y} : {fx: fix[0], fy: fix[1]}"}
-       {:trigger "!fix", :modify "node", :values "{fx: null, fy: null}"}],
+      [{:trigger "fix" :modify "node" :values "fix === true ? {fx: node.x, fy: node.y} : {fx: fix[0], fy: fix[1]}"}
+       {:trigger "!fix" :modify "node" :values "{fx: null, fy: null}"}]
       :encode
-      {:enter {:fill {:scale "color", :field "group"}, :stroke {:value "white"}, :size {:field "size"}},
-       :update {:size {:signal "2 * nodeRadius * nodeRadius"}, :cursor {:value "pointer"}}
-       },
+      {:enter {:fill {:scale "color" :field "group"}
+               :stroke {:value "white"}
+               :size {:field "size"}}
+       #_ :update #_ {:size {:signal "2 * nodeRadius * nodeRadius"} :cursor {:value "pointer"}}
+       }
       :transform
-      [{:type "force",
-        :iterations 300,
-        :restart {:signal "restart"},
-        :static {:signal "static"},
-        :signal "force",
+      [{:type "force"
+        :iterations 300
+        :restart {:signal "restart"}
+        :static {:signal "static"}
+        :signal "force"
         :forces
-        [{:force "center", :x {:signal "cx"}, :y {:signal "cy"}}
-         {:force "collide", :radius {:signal "nodeRadius"}}
-         {:force "nbody", :strength {:signal "nodeCharge"}}
-         {:force "link", :links "link-data", :distance {:signal "linkDistance"}}]}]}
-     {:type "path",
-      :from {:data "link-data"},
-      :interactive false,
-      :encode {:update {:stroke {:value "#ccc"}, :strokeWidth {:value 0.5}}},
+        [{:force "center" :x {:signal "cx"} :y {:signal "cy"}}
+         {:force "collide" :radius {:signal "nodeRadius"}}
+         {:force "nbody" :strength {:signal "nodeCharge"}}
+         {:force "link" :links "link-data" :distance {:signal "linkDistance"}}]}]}
+     {:type "path"
+      :from {:data "link-data"}
+      :interactive false
+      :encode {:update {:stroke {:value "#ccc"} :strokeWidth {:value 0.5}}}
       :transform
-      [{:type "linkpath",
-        :require {:signal "force"},
-        :shape "line",
-        :sourceX "datum.source.x",
-        :sourceY "datum.source.y",
-        :targetX "datum.target.x",
-        :targetY "datum.target.y"}]}],
-    :$schema "https://vega.github.io/schema/vega/v5.json",
+      [{:type "linkpath"
+        :require {:signal "force"}
+        :shape "line"
+        :sourceX "datum.source.x"
+        :sourceY "datum.source.y"
+        :targetX "datum.target.x"
+        :targetY "datum.target.y"}]}
+     {:type "text"
+      :interactive false
+
+      :encode {
+               :enter {:x {:value 600}
+                       :y {:value 50}
+                       :fill {:value "black"}
+                       :fontSize {:value 20}
+                       :align {:value "right"}
+                       }
+               :update {:text {:signal "title"}}
+               }
+      }
+     ]
     :signals
-    [{:name "cx", :update "width / 2"}
-     {:name "cy", :update "height / 2"}
-     {:name "nodeRadius", :value 8, :bind {:input "range", :min 1, :max 50, :step 1}}
-     {:name "nodeCharge", :value -30, :bind {:input "range", :min -100, :max 10, :step 1}}
-     {:name "linkDistance", :value 30, :bind {:input "range", :min 5, :max 100, :step 1}}
-     {:name "static", :value true, :bind {:input "checkbox"}}
-     {:description "State variable for active node fix status.",
-      :name "fix",
-      :value false,
+    [{:name "cx" :update "width / 2"}
+     {:name "cy" :update "height / 2"}
+     {:name "hover"
+      :value nil
+      :on [
+           {:events "@nodes:mouseover" :update "datum"}
+           {:events "@nodes:mouseout" :update "null"}
+           ]
+      }
+     {:name "title"
+;      :value "AMMDIx"
+      :update "hover ? hover.name : 'AMMDI'"
+      }
+     {:name "nodeRadius" :value 8 :bind {:input "range" :min 1 :max 50 :step 1}}
+     {:name "nodeCharge" :value -30 :bind {:input "range" :min -100 :max 10 :step 1}}
+     {:name "linkDistance" :value 30 :bind {:input "range" :min 5 :max 100 :step 1}}
+     {:name "static" :value true :bind {:input "checkbox"}}
+     {:description "State variable for active node fix status."
+      :name "fix"
+      :value false
       :on
-      [{:events "symbol:mouseout[!event.buttons], window:mouseup", :update "false"}
-       {:events "symbol:mouseover", :update "fix || true"}
-       {:events "[symbol:mousedown, window:mouseup] > window:mousemove!",
-        :update "xy()",
+      [{:events "symbol:mouseout[!event.buttons], window:mouseup" :update "false"}
+       {:events "symbol:mouseover" :update "fix || true"}
+       {:events "[symbol:mousedown, window:mouseup] > window:mousemove!"
+        :update "xy()"
         :force true}]}
-     {:description "Graph node most recently interacted with.",
-      :name "node",
-      :value nil,
-      :on [{:events "symbol:mouseover", :update "fix === true ? item() : node"}]}
-     {:description "Flag to restart Force simulation upon data changes.",
-      :name "restart",
-      :value false,
-      :on [{:events {:signal "fix"}, :update "fix && fix.length"}]}],
-    :height 500,
+     {:description "Graph node most recently interacted with."
+      :name "node"
+      :value nil
+      :on [{:events "symbol:mouseover" :update "fix === true ? item() : node"}]}
+     {:description "Flag to restart Force simulation upon data changes."
+      :name "restart"
+      :value false
+      :on [{:events {:signal "fix"} :update "fix && fix.length"}]}]
+    :height 500
     })
 
 (defn display
   [block-map]
-  (oz/view! (spec block-map) :port 1884))
+  (oz/view! (spec block-map) :port 1884 :mode :vega))
 
