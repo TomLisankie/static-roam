d6993af5d22df806cdd64bba7469e9ca0d3af96a
Mike Travers <mtravers@parkerici.org>
2021-09-19 17:19:59 -0700

    tweaks for tutorial

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,3 +1,4 @@
+source bin/setup.sh
 echo "Generating $ROAM_API_GRAPH"
 lein run $ROAM_CONFIG
 
--- bin/setup-example.sh
+++ bin/setup-example.sh
@@ -3,7 +3,7 @@
 export ROAM_CONFIG=mysite.edn # your config .edn file
 export ROAM_OUTPUT_DIR=output # output directory
 
-# Credentials to fetch the Roam graph
+# Credentials to fetch the Roam graph. If you are going to export in manually, you can omit these.
 # This mechanism won't work if you log into Roam with a 3rd party authorization (Google etc).
 export ROAM_API_GRAPH=my-graph	   # Roam graph name
 export ROAM_API_EMAIL=me@gmail.com # Roam login account name
